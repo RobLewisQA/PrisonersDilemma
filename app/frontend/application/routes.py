8ad5b6b44b1ff262b6248dddc570482f9dcf1f22
RobLewisQA <rlewis@academytrainee.com>
2021-08-18 15:11:59 +0000

    added pages to pd tutorial

--- app/frontend/application/routes.py
+++ app/frontend/application/routes.py
@@ -4,30 +4,34 @@ import pandas as pd
 import requests
 
 @app.route('/', methods=['GET','POST']) 
-def home_fe():
+def home():
     return render_template("homepage.html")#"wanna play?" + '<br><br><a href="/1">Start a tournament?</a> </br>'
 
-@app.route('/1', methods=['GET','POST']) 
-def singlegame_fe():
-    return 'singlegame_fe'
+@app.route('/tutorial1', methods=['GET','POST']) 
+def tutorial_1():
+    return render_template("tutorial_1.html")
 
-@app.route('/2', methods=['GET','POST']) 
-def fiveturngame_fe():
-    return 'fiveturngame_fe'
 
-@app.route('/3', methods=['GET','POST']) 
-def fiveturngameroundrobin_fe():
-    return 'fiveturngameroundrobin_fe'
+# def singlegame_fe():
+#     return 'singlegame_fe'
 
-@app.route('/4', methods=['GET','POST']) 
-def fiftyturngameroundrobin_fe():
-    return 'fiftyturngameroundrobin_fe'
+# @app.route('/2', methods=['GET','POST']) 
+# def fiveturngame_fe():
+#     return 'fiveturngame_fe'
 
-@app.route('/5', methods=['GET','POST']) 
-def tournamentwitherror_fe():
-    return 'tournamentwitherror_fe'
+# @app.route('/3', methods=['GET','POST']) 
+# def fiveturngameroundrobin_fe():
+#     return 'fiveturngameroundrobin_fe'
 
-@app.route('/6', methods=['GET','POST']) 
-def tournamentwithspawning_fe():
-    return 'tournamentwithspawning_fe'
+# @app.route('/4', methods=['GET','POST']) 
+# def fiftyturngameroundrobin_fe():
+#     return 'fiftyturngameroundrobin_fe'
+
+# @app.route('/5', methods=['GET','POST']) 
+# def tournamentwitherror_fe():
+#     return 'tournamentwitherror_fe'
+
+# @app.route('/6', methods=['GET','POST']) 
+# def tournamentwithspawning_fe():
+#     return 'tournamentwithspawning_fe'
 
